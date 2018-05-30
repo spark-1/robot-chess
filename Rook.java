518e9a4ca21afc17c8a26c7289a1037aaf3b5946
CBI278 <changbinpersona@gmail.com>
2018-05-30 15:11:41 +0900

    룩을 바꿔보았습니다. 확정은 아닙니다!

--- Rook.java
+++ Rook.java
@@ -1,14 +1,28 @@
-public class Rook extends Base implements Rookable{
+package model;
 
-	Rookable rk = null;
+public class Rook extends Base implements Rookable{
+	//컴응 Ready에 있는 동물원 동물을 바탕으로 만들었습니다.
+	Rookable rk = null; //Rookable은 왜 있는지 의문입니다... 만 일단 교수님이 넣어놓으셨어서 넣었습니다.
+	public Siegeable se = null; //Siegeable도
+	public Movable mv = null; //Movable도 null 지금은 그 어떤것도 아닙니다.
 	
 	public Rook(String name, String type){
     	super(name, type);
     	System.out.println("Rook 생성");
+    	mv = (Movable)this; //생성시 Movable을 Rook으로 바꿔줍니다.
+    	se = null; //Siegeable은 null이고요. 이러면 지금 Rook은 Movable입니다.
+    	rk = (Rookable)mv;
     }
 
     public void move(){
-    		System.out.println("Rook 움직임");
+    	if(this.mv == null) //mv가 null, 즉 se가 null이 아니라 움직이질 못합니다.
+		{
+			System.out.println("못 움직여요! Siegeable이에오!");
+		}
+		else
+		{
+			System.out.println("Rook 움직여요!");
+		}
     }
 
 	@Override
@@ -18,19 +32,37 @@ public class Rook extends Base implements Rookable{
 
 	@Override
 	public void status_change() {
-		// TODO Auto-generated method stub
 		
 	}
 
 	@Override
 	public void attack() {
-		// TODO Auto-generated method stub
-	
+		if(this.mv == null) //mv가 null, 즉 se가 null이 아니라 공격이 가능합니다.
+		{
+			System.out.println("Rook 공격해요!");
+		}
+		else
+		{
+			System.out.println("못 공격해요! Movable이에요!");
+		}
 	}
 
 	@Override
 	public void skill() {
-		rk.skill();
+		if(this.mv == null) //mv가 null, 즉 se가 null이 아니라 Skill을 쓰면 Movable이 됩니다.
+		{
+			this.se = null; //se가 null
+			this.mv = this; //Rook이 mv로 바뀝니다.
+			rk = (Rookable)mv;
+			System.out.println("이젠 Movable이에요!");
+		}
+		else
+		{
+			this.mv = null;
+			this.se = this; //반대의 경우에는 반대로.
+			rk = (Rookable)se;
+			System.out.println("이젠 Siegeable이에요!");
+		}
 	}
 
 }
